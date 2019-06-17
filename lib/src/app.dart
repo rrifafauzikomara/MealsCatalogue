3c2e134ea7139e57ac46930a58ddc16a6c2f9a16
rrifafauzikomara <rifafauzi6@gmail.com>
2019-06-18 00:37:27 +0700

    add toast, fix UI, fix sub3

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 
 import 'package:dicoding_submission/src/view/seafood_screen.dart';
 import 'package:dicoding_submission/src/view/desert_screen.dart';
@@ -25,60 +26,64 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   int _page = 0;
+  final List<Widget> _children = [
+    DesertScreen(),
+    SeafoodScreen()];
 
   @override
   Widget build(BuildContext context) {
     String title = "The Meals";
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            title,
-            style: TextStyle(color: Colors.white),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          title,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          _children[_page],
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Theme(
+                  data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+                  child: CurvedNavigationBar(
+                    index: 0,
+                    height: 50.0,
+                    items: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(5.0),
+                        child: Icon(Icons.free_breakfast, size: 30),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(5.0),
+                        child: Icon(Icons.fastfood, size: 30),
+                      ),
+                    ],
+                    color: Colors.white,
+                    buttonBackgroundColor: Colors.white,
+                    backgroundColor: Colors.transparent,
+                    animationCurve: Curves.easeInOut,
+                    animationDuration: Duration(milliseconds: 600),
+                    onTap: onTabTapped,
+                  )
+              )
           ),
-        ),
-        bottomNavigationBar: CurvedNavigationBar(
-          index: 0,
-          height: 50.0,
-          items: <Widget>[
-            Container(
-              padding: EdgeInsets.all(5.0),
-              child: Icon(Icons.free_breakfast, size: 30),
-            ),
-            Container(
-              padding: EdgeInsets.all(5.0),
-              child: Icon(Icons.fastfood, size: 30),
-            ),
-          ],
-          color: Colors.white,
-          buttonBackgroundColor: Colors.white,
-          backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
-          animationCurve: Curves.easeInOut,
-          animationDuration: Duration(milliseconds: 600),
-          onTap: (index) {
-            switch (index) {
-              case 0:
-                setState(() {
-                  _page = 0;
-                });
-                break;
-              case 1:
-                setState(() {
-                  _page = 1;
-                });
-                break;
-            }
-          },
-        ),
-        body: _page == 0
-            ? DesertScreen()
-            : SeafoodScreen());
+        ],
+      ),
+    );
   }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _page = index;
+
+    });
+  }
+
 }
 
-showSnackBar(BuildContext context, String mealsName) {
-  final snackBar = SnackBar(
-    content: Text(mealsName)
-  );
-  Scaffold.of(context).showSnackBar(snackBar);
+void showToast(BuildContext context, String mealsName,  {int duration, int gravity}) {
+  Toast.show(mealsName, context, duration: duration, gravity: gravity);
 }
--- lib/src/view/detail_screen.dart
+++ lib/src/view/detail_screen.dart
@@ -6,16 +6,17 @@ import 'package:dicoding_submission/src/models/meals_detail.dart';
 class DetailScreen extends StatefulWidget {
 
   final String idMeal;
+  final String strMeal;
   final String strMealThumb;
 
-  const DetailScreen({Key key, @required this.idMeal, this.strMealThumb}) : super(key: key);
+  const DetailScreen({Key key, @required this.idMeal, this.strMeal, this.strMealThumb}) : super(key: key);
 
   @override
   _DetailScreenState createState() => _DetailScreenState();
 
 }
 
-class _DetailScreenState extends State<DetailScreen> {
+class _DetailScreenState extends State<DetailScreen> with TickerProviderStateMixin{
 
   @override
   void initState() {
@@ -32,17 +33,33 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          iconTheme: IconThemeData(
-            color: Colors.white,
-          ),
-          title: Text(
-            'Detail Meals',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        body: getListDetail()
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.transparent,
+              expandedHeight: 270,
+              floating: false,
+              pinned: true,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Hero(
+                  tag: widget.strMeal,
+                  child: Material(
+                    child: InkWell(
+                      child: Image.network(
+                          widget.strMealThumb,
+                          width: double.infinity,
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ];
+        },
+        body: getListDetail(),
+      ),
     );
   }
 
@@ -52,9 +69,6 @@ class _DetailScreenState extends State<DetailScreen> {
         builder: (context, AsyncSnapshot<MealsDetail> snapshot) {
           if (snapshot.hasData) {
             return _showListDetail(
-                snapshot.data.meals[0].idMeal,
-                snapshot.data.meals[0].strMeal,
-                snapshot.data.meals[0].strMealThumb,
                 snapshot.data.meals[0].strCategory,
                 snapshot.data.meals[0].strArea,
                 snapshot.data.meals[0].strIngredient1,
@@ -73,9 +87,6 @@ class _DetailScreenState extends State<DetailScreen> {
   }
 
   Widget _showListDetail(
-      String id,
-      String name,
-      String image,
       String category,
       String area,
       String ingredient1,
@@ -87,19 +98,11 @@ class _DetailScreenState extends State<DetailScreen> {
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          Hero(
-            tag: name,
-            child: Material(
-              child: InkWell(
-                child: Image.network(image, fit: BoxFit.cover),
-              ),
-            ),
-          ),
           Container(
             alignment: Alignment.center,
             padding: EdgeInsets.all(3.0),
             child: Text(
-              name,
+              widget.strMeal,
               style: TextStyle(
                   fontSize: 17,
                   fontWeight: FontWeight.bold,
@@ -216,199 +219,3 @@ class _DetailScreenState extends State<DetailScreen> {
     );
   }
 }
-
-//class DetailScreen extends StatelessWidget {
-//
-//  final String id;
-//
-//  DetailScreen({Key key, @required this.id}) : super(key: key);
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    bloc.fetchDetailMeals(id);
-//    return Scaffold(
-//        appBar: AppBar(
-//          centerTitle: true,
-//          iconTheme: IconThemeData(
-//            color: Colors.white,
-//          ),
-//          title: Text(
-//            'Detail Meals',
-//            style: TextStyle(color: Colors.white),
-//          ),
-//        ),
-//        body: getListDetail()
-//    );
-//  }
-//
-//  getListDetail() {
-//    return StreamBuilder(
-//        stream: bloc.detailMeals,
-//        builder: (context, AsyncSnapshot<MealsDetail> snapshot) {
-//          if (snapshot.hasData) {
-//            return _showListDetail(
-//                snapshot.data.meals[0].idMeal,
-//                snapshot.data.meals[0].strMeal,
-//                snapshot.data.meals[0].strMealThumb,
-//                snapshot.data.meals[0].strCategory,
-//                snapshot.data.meals[0].strArea,
-//                snapshot.data.meals[0].strIngredient1,
-//                snapshot.data.meals[0].strIngredient2,
-//                snapshot.data.meals[0].strIngredient3,
-//                snapshot.data.meals[0].strIngredient4,
-//                snapshot.data.meals[0].strIngredient5,
-//                snapshot.data.meals[0].strInstructions);
-//          } else if (snapshot.hasError) {
-//            return Text(snapshot.error.toString());
-//          }
-//          return Center(child: CircularProgressIndicator(
-//              valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(58, 66, 86, 1.0),)
-//          ));
-//        });
-//  }
-//
-//  Widget _showListDetail(
-//      String id,
-//      String name,
-//      String image,
-//      String category,
-//      String area,
-//      String ingredient1,
-//      String ingredient2,
-//      String ingredient3,
-//      String ingredient4,
-//      String ingredient5,
-//      String desc) {
-//    return SingleChildScrollView(
-//      child: Column(
-//        children: <Widget>[
-//          Hero(
-//            tag: name,
-//            child: Material(
-//              child: InkWell(
-//                child: Image.network(image, fit: BoxFit.cover),
-//              ),
-//            ),
-//          ),
-//          Container(
-//            alignment: Alignment.center,
-//            padding: EdgeInsets.all(3.0),
-//            child: Text(
-//              name,
-//              style: TextStyle(
-//                  fontSize: 17,
-//                  fontWeight: FontWeight.bold,
-//                  color: Colors.black),
-//            ),
-//          ),
-//          Container(
-//            padding: EdgeInsets.all(5.0),
-//            child: Row(
-//              children: <Widget>[
-//                Align(
-//                  alignment: Alignment.centerLeft,
-//                  child: Text(
-//                    "Category : ",
-//                    style: TextStyle(
-//                        fontSize: 15,
-//                        fontWeight: FontWeight.bold,
-//                        color: Colors.black),
-//                  ),
-//                ),
-//                Align(
-//                  alignment: Alignment.centerLeft,
-//                  child: Text(
-//                    category,
-//                    style: TextStyle(
-//                        fontStyle: FontStyle.normal, color: Colors.black),
-//                  ),
-//                ),
-//              ],
-//            ),
-//          ),
-//          Container(
-//            padding: EdgeInsets.all(5.0),
-//            child: Row(
-//              children: <Widget>[
-//                Align(
-//                  alignment: Alignment.centerLeft,
-//                  child: Text(
-//                    "Area : ",
-//                    style: TextStyle(
-//                        fontSize: 15,
-//                        fontWeight: FontWeight.bold,
-//                        color: Colors.black),
-//                  ),
-//                ),
-//                Align(
-//                  alignment: Alignment.centerLeft,
-//                  child: Text(
-//                    area,
-//                    style: TextStyle(
-//                        fontStyle: FontStyle.normal, color: Colors.black),
-//                  ),
-//                ),
-//              ],
-//            ),
-//          ),
-//          Container(
-//            padding: EdgeInsets.all(5.0),
-//            child: Column(
-//              children: <Widget>[
-//                Align(
-//                  alignment: Alignment.centerLeft,
-//                  child: Text(
-//                    "Ingredient :",
-//                    style: TextStyle(
-//                        fontSize: 15,
-//                        fontWeight: FontWeight.bold,
-//                        color: Colors.black),
-//                  ),
-//                ),
-//                Align(
-//                  alignment: Alignment.centerLeft,
-//                  child: Text(
-//                    ingredient1+ ', ' +
-//                        ingredient1 + ', ' +
-//                        ingredient2 + ', ' +
-//                        ingredient3 + ', ' +
-//                        ingredient4 + ', ' +
-//                        ingredient5,
-//                    style: TextStyle(
-//                        fontStyle: FontStyle.italic, color: Colors.black),
-//                  ),
-//                ),
-//              ],
-//            ),
-//          ),
-//          Container(
-//            padding: EdgeInsets.all(5.0),
-//            child: Column(
-//              children: <Widget>[
-//                Align(
-//                  alignment: Alignment.centerLeft,
-//                  child: Text(
-//                    "Instructions :",
-//                    style: TextStyle(
-//                        fontSize: 15,
-//                        fontWeight: FontWeight.bold,
-//                        color: Colors.black),
-//                  ),
-//                ),
-//                Align(
-//                  alignment: Alignment.centerLeft,
-//                  child: Text(
-//                    desc,
-//                    style: TextStyle(
-//                        fontStyle: FontStyle.italic, color: Colors.black),
-//                  ),
-//                ),
-//              ],
-//            ),
-//          ),
-//        ],
-//      ),
-//    );
-//  }
-//
-//}
